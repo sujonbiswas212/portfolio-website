b64719fd734ed55968bb72d4bfca46e1907a13c6
sujonbiswas212 <wwwsujonbiswas212@gmail.com>
2025-03-17 04:14:59 +0600

    first commit

---
+++ animation.js
@@ -0,0 +1,56 @@
+ScrollReveal().reveal('.hero-text', {
+     delay: 100,
+     duration: 1000,
+     origin:'left',
+     distance: '100px',
+     reset: true,
+     Interval:200
+ });
+
+
+ ScrollReveal().reveal('.bastop-text', {
+     delay: 100,
+     duration: 1000,
+     origin:'top',
+     distance: '50px',
+     reset: true,
+     Interval:200
+ });
+
+
+ ScrollReveal().reveal('.btn', {
+     delay: 100,
+     duration: 1000,
+     origin:'right',
+     distance: '50px',
+     reset: true,
+     Interval:200
+ });
+
+ ScrollReveal().reveal('.bastob-img', {
+     delay: 100,
+     duration: 1000,
+     origin:'right',
+     distance: '50px',
+     reset: true,
+     Interval:200
+ });
+
+
+ ScrollReveal().reveal('.friend', {
+    delay: 100,
+    duration: 1000,
+    origin:'bottom',
+    distance: '50px',
+    reset: true,
+    Interval:300
+});
+
+ScrollReveal().reveal('.friend h2', {
+    delay: 100,
+    duration: 1000,
+    origin:'left',
+    distance: '50px',
+    reset: true,
+    Interval:300
+});
